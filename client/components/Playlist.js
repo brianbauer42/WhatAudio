b47f72ac7b1662f22733081e68eff0e78f981ef9
bbauer-io <brian.bauer@inventati.org>
2017-08-27 13:03:32 -0700

    pulled track cards out of playlist and made them their own components

--- client/components/Playlist.js
+++ client/components/Playlist.js
@@ -1,5 +1,8 @@
 import React, { Component } from 'react';
 import axios from 'axios';
+import StandardTrackCard from './TrackCards/StandardTrackCard';
+import StandardEditTrackCard from './TrackCards/StandardEditTrackCard';
+import StandardDeleteTrackCard from './TrackCards/StandardDeleteTrackCard';
 
 class Playlist extends Component {
   constructor(props) {
@@ -10,9 +13,12 @@ class Playlist extends Component {
         tracksBeingDeleted: []
       }
 
-      this.standardTrackCard = this.standardTrackCard.bind(this);
-      this.editingTrackCard = this.editingTrackCard.bind(this);
-      this.deletingTrackCard = this.deletingTrackCard.bind(this);
+      this.handleEditPost = this.handleEditPost.bind(this);
+      this.handleDeletePost = this.handleDeletePost.bind(this);
+      this.toggleDeleteTrackCard = this.toggleDeleteTrackCard.bind(this);
+      this.toggleEditTrackCard = this.toggleEditTrackCard.bind(this);
+      this.handleTrackSelection = this.handleTrackSelection.bind(this);
+      this.generateClassName = this.generateClassName.bind(this);
       this.handleInputChange = this.handleInputChange.bind(this);
   }
 
@@ -93,95 +99,39 @@ class Playlist extends Component {
       this.setState({ tracksBeingDeleted: this.state.tracksBeingDeleted.concat([track._id]) });
     }
   }
-
-  standardTrackCard(track) {
-    return (
-      <div className={this.generateClassName(track)} key={track._id} onClick={(e) => this.handleTrackSelection(track, e)} >
-        <div className="artContainer">
-          <img className="albumArt" src={track.artUri !== undefined ? 'resources/' + track.artUri : '/favicon.ico'} alt="album cover" />
-        </div>
-        <div className="postBody">
-          <h4 className="trackTitle">{track.artist} - {track.title}</h4>
-          <h5 className="albumTitle">{track.album}</h5>
-          <p className="postBodyText">{track.postBody}</p>
-          <p className="postAuthor">- {track.sharedBy.displayName}</p>
-        </div>
-        {this.props.isLoggedIn() ? 
-        <div className="postButtonsContainer">
-          <i className='fa fa-pencil-square-o fa-lg colorClick postEditButton' onClick={this.toggleEditTrackCard.bind(this, track) } />
-          <i className='fa fa-times fa-lg colorClick postDeleteButton' onClick={this.toggleDeleteTrackCard.bind(this, track) } />
-        </div>
-          : null }
-      </div>
-    )
-  }
-
-  deletingTrackCard(track) {
-    return (
-      <div className={this.generateClassName(track)} key={track._id} >
-        <div className="artContainer" onClick={(e) => this.handleTrackSelection(track, e)} >
-          <img className="albumArt" src={track.artUri !== undefined ? 'resources/' + track.artUri : '/favicon.ico'} alt="album cover" />
-        </div>
-        <div className="postBody">
-          <h2 className="areYouSure">Are you sure you want to delete this track?</h2>
-          <h3 className="trackTitle">{track.artist} - {track.title}</h3><p className="postAuthor">Shared by: {track.sharedBy.displayName}</p>
-          <button type="button" className="redButton" onClick={this.handleDeletePost.bind(this, track._id)} >Delete!</button>
-          <button type="button" className="greenButton" onClick={this.toggleDeleteTrackCard.bind(this, track) }>Keep!</button>
-        </div>
-      </div>
-    )
-  }
-
-  editingTrackCard(track) {
-    return (
-      <div className={this.generateClassName(track)} key={track._id} >
-        <div className="artContainer" onClick={(e) => this.handleTrackSelection(track, e)} >
-          <img className="albumArt" src={track.artUri !== undefined ? 'resources/' + track.artUri : '/favicon.ico'} alt="album cover" />
-        </div>
-        <div className="postBody">
-          <form id="trackUpdateForm" className="trackUpdateForm" onSubmit={this.handleTrackUpdate}>
-            <h5>Artist: 
-              <input type='text' 
-                value={this.state[track._id].artist}
-                onChange={(e) => this.handleInputChange('artist', track._id, e)}
-              /> Title:  
-              <input type='text' 
-                value={this.state[track._id].title}
-                onChange={(e) => this.handleInputChange('title', track._id, e)}
-              />
-            </h5>
-            <h5>Album: 
-              <input type='text' 
-                value={this.state[track._id].album}
-                onChange={(e) => this.handleInputChange('album', track._id, e)}
-              />
-            </h5>
-            <input type='textarea' 
-              value={this.state[track._id].postBody}
-              onChange={(e) => this.handleInputChange('postBody', track._id, e)}
-            />
-            <p className="postAuthor">- {track.sharedBy.displayName}</p>
-            <button type="submit" onClick={this.handleEditPost.bind(this, track)} >Save Changes</button>
-          </form>
-        </div>
-        <div className="postButtonsContainer">
-          <i className='fa fa-pencil-square-o fa-lg colorClick postEditButton' onClick={this.toggleEditTrackCard.bind(this, track) } />
-          <i className='fa fa-times fa-lg colorClick postDeleteButton' onClick={this.toggleDeleteTrackCard.bind(this, track._id)} />
-        </div>
-      </div>
-    )
-  }
   
   trackCards() {
     if (this.props.posts) {
       return (
         this.props.posts.map(track => {
-          if (this.state.tracksBeingDeleted.includes(track._id))
-            return (this.deletingTrackCard(track));
-          else if (this.state.tracksBeingEdited.includes(track._id))
-            return (this.editingTrackCard(track));
-          else {
-            return this.standardTrackCard(track);
+          if (this.state.tracksBeingDeleted.includes(track._id)) {
+            return <StandardDeleteTrackCard key={track._id}
+                                            track={track}
+                                            generateClassName={this.generateClassName}
+                                            handleTrackSelection={this.handleTrackSelection}
+                                            toggleDeleteTrackCard={this.toggleDeleteTrackCard}
+                                            handleDeletePost={this.handleDeletePost}
+                                            />
+          } else if (this.state.tracksBeingEdited.includes(track._id)) {
+            return <StandardEditTrackCard key={track._id}
+                                          track={track}
+                                          inputState={this.state[track._id]}
+                                          generateClassName={this.generateClassName}
+                                          handleTrackSelection={this.handleTrackSelection}
+                                          toggleDeleteTrackCard={this.toggleDeleteTrackCard}
+                                          toggleEditTrackCard={this.toggleEditTrackCard}
+                                          handleInputChange={this.handleInputChange}
+                                          handleEditPost={this.handleEditPost}
+                                          />
+          } else {
+            return <StandardTrackCard key={track._id} 
+                                      track={track}
+                                      generateClassName={this.generateClassName}
+                                      handleTrackSelection={this.handleTrackSelection}
+                                      isLoggedIn={this.props.isLoggedIn}
+                                      toggleDeleteTrackCard={this.toggleDeleteTrackCard}
+                                      toggleEditTrackCard={this.toggleEditTrackCard}
+                                      />
           }
         })
       )
